af6cbecb7f9f05485bd167b315fd2c19aa30914c
joser <jose.reyesfabian@gmail.com>
2026-05-18 17:21:25 -0400

    51826

---
+++ src/test/java/com/jose/apispring/ApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jose.apispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
