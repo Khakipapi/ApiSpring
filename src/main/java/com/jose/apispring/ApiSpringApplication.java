844929fbac2457fd23eed5817c448562f1c32bb8
joser <jose.reyesfabian@gmail.com>
2026-05-19 12:13:52 -0400

    51926

--- src/main/java/com/jose/apispring/ApiSpringApplication.java
+++ src/main/java/com/jose/apispring/ApiSpringApplication.java
@@ -2,12 +2,20 @@ package com.jose.apispring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ApiSpringApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(ApiSpringApplication.class, args);
+  }
+
+  @GetMapping
+  public String helloWorld() {
+    return "Hello World!";
   }
 
 }
---
+++ src/main/java/com/jose/apispring/SoftwareEngineer.java
@@ -0,0 +1,5 @@
+package com.jose.apispring;
+
+public class SoftwareEngineer {
+
+}
